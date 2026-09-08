7d0f16be79efdb4922da4a0815a12b32cb67d38d
diogorsilva1806-cell <diogorsilva1806@gmail.com>
2026-09-08 15:45:46 +0100

    2

--- BackEnd/obj/Debug/net10.0/MinhaPokeAPI.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net10.0/MinhaPokeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaPokeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+433c2b5daa98c09c0e6f4974b8905ee9fb6f89f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+509c8e941f1ae619f266a191c9f54afc667f9503")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaPokeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaPokeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
