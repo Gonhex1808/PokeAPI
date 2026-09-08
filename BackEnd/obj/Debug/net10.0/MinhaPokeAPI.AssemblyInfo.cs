ce8278952c78e325cd3ab4fe43daa1e491d6ca69
Mário_Fortunato_808 <theberserker815@gmail.com>
2026-09-08 16:27:14 +0100

    cache 4

--- BackEnd/obj/Debug/net10.0/MinhaPokeAPI.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net10.0/MinhaPokeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaPokeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+445503ccec9ec454c8f2c024ad916112fb3878d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+682594982e0588b0908dc70535afa3f167b919ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaPokeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaPokeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
